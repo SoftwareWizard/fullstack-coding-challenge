1cf722dbd1c20c7d2d57acff81274d2084a937b3
Simon Bauer <simon@software-wizard.tech>
2021-09-25 10:35:15 -0500

    fix TestBed

--- src/app/core/containers/app/app.component.spec.ts
+++ src/app/core/containers/app/app.component.spec.ts
@@ -1,9 +1,22 @@
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+
+import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    // await TestBed.configureTestingModule({ imports: [RouterTestingModule], declarations: [AppComponent] }).compileComponents();
+  let component: AppComponent;
+  let spectator: Spectator<AppComponent>;
+
+  const createComponent = createComponentFactory({
+    component: AppComponent
   });
 
-  it('should create the app', () => {
-    expect(true).toBeTruthy();
+  beforeEach(() => {
+    spectator = createComponent();
+    component = spectator.component;
+  });
+
+  it('should create the component', async () => {
+    expect(component).toBeTruthy();
+    expect(spectator.query('h2')).toHaveText('Hurra');
   });
 });
--- src/setupJest.ts
+++ src/setupJest.ts
@@ -1,5 +1,14 @@
-import 'jest-preset-angular';
+// import 'zone.js';
+// import 'zone.js/testing';
+import 'jest-preset-angular/setup-jest';
 import 'jest-chain';
+
+// import { TestBed } from '@angular/core/testing';
+// import {
+//     BrowserDynamicTestingModule, platformBrowserDynamicTesting
+// } from '@angular/platform-browser-dynamic/testing';
+
+// TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
 Object.defineProperty(window, 'CSS', {value: null});
 Object.defineProperty(window, 'getComputedStyle', {
